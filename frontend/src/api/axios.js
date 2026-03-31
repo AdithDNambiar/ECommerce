f914d0608401287a782f874df37ad9491c151513
Adith Nambiar <adiadithdnambiar@gmail.com>
2026-03-31 12:05:48 +0530

    deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://shopx-backend-sp5w.onrender.com/api",
   withCredentials: true
 });
 
